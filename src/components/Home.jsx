6d854ba530e96c8964c2a275f0d77b53a356da4d
sougata <sougataghar47@gmail.com>
2025-02-15 15:38:35 +0530

    first push

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+export default function Home() {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/api");
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+}
