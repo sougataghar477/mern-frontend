5839ef2140e944387b72612854be107cd5f606c9
sougata <sougataghar47@gmail.com>
2025-02-15 17:08:51 +0530

    2nd push

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://mern-backend-gznwfusrj-sougataghar477s-projects.vercel.app',
       },
     }
   }
